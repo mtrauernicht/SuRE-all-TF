d177edea9a0be3aebd310faf9a98d2984a4a8991
m.trauernicht <m.trauernicht@nki.nl>
2020-09-24 11:37:50 +0200

    moved files

--- TF-reporter_library_design/mt20200221_kmers.py
+++
@@ -1,7 +0,0 @@
-import itertools
-
-combinations = itertools.product(*itertools.repeat(['A','T','C','G'], 14))
-
-for i in combinations:
-    kmers = (''.join(i))
-    print(">"+''.join(i), "\n", kmers)
